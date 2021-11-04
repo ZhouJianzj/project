98e6ea0cae7a32c2871556ddd171135c86d10cf9
ZhouJianzj <2998390669@qq.com>
2021-11-04 09:50:09 +0800

    完成sys模块

--- common-api/src/main/java/com/zj/interceptor/CommoInterceptor.java
+++ common-api/src/main/java/com/zj/interceptor/CommoInterceptor.java
@@ -1,6 +1,7 @@
 package com.zj.interceptor;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.zj.entity.UserManager;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,9 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Map;
 
 public class CommoInterceptor implements HandlerInterceptor {
 
@@ -21,11 +24,16 @@ public class CommoInterceptor implements HandlerInterceptor {
         if ( session == null){
 
             // 响应结果，响应数据为JSON数据
-            HashMap data = new HashMap();
-            DataBuffer buffer = null;
-                data.put("code", 404);
-                data.put("msg", "no login, please login!");
-                response.getWriter().write(String.valueOf(data));
+
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("json; charset=utf-8");
+            JSONObject o = new JSONObject();
+            PrintWriter writer = response.getWriter();
+            o.put("stuts","400");
+            o.put("msg","没用登录！");
+            writer.print(o);
+
+
            return false;
        }else {
             UserManager user = (UserManager) session.getAttribute("user");
