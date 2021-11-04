38220c218e9f6d75ae519e446b4a821923263ec6
your name <your email>
2021-11-04 15:38:49 +0800

    item 模块1.0

---
+++ item-manager/src/main/java/com/zj/ItemManagerApplication.java
@@ -0,0 +1,22 @@
+package com.zj;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * @author dell
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableRedisHttpSession
+@MapperScan("com.zj.dao")
+public class ItemManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemManagerApplication.class, args);
+    }
+
+}
