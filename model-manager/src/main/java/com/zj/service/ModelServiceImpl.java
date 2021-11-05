41f3528b677a2c4f61521e72d9d04a480116d16b
ZhouJianzj <2998390669@qq.com>
2021-11-05 12:15:38 +0800

    /**  * @author zhoujian  * 方法名命名规范  * -------------------------------------------------------------------------------  * url规范：  * 例如：  *      管道管理的所用url根都是  *      /pipe  *      如果根据管道id查询管道  *      /pipe/id  * -------------------------------------------------------------------------------  * controller层类中方法命名规范  * 使用规范的请求：  *      PostMapping(新增) DeleteMapping(删除) GetMapping(查询) PutMapping(修改)  * 动词：  *      查询 find  *      添加 add  *      删除 delete  *      修改 modify  * 例如：  * 添加管道的控制器方法           add  +  pipe  + controller = addPipeController  * 根据id查询管道的控制器方法      find + pipe  + id + controller = findPipeIdController  * 删除指定id的管道控制器方法      delete + pipe  + id + controller = deletePipeIdController  * 修改指定id的官管道的控制器方法   modfiy + pipe  + id + controller = modfiyPipeIdController  * ------------------------------------------------------------------------------  * service层类中方法名规范： 动词与控制器中的相同  * 例如：  *      addPipeController ------>  addPipeService  * ------------------------------------------------------------------------------  * dao层类方法命名规范：  * 动词：  *      查询：select  *      添加：insert  *      删除：delete  *      修改：update  *  *例如：  *      添加管道：  *      addPiepService --------->pipeInsert  *      pipe + insert = pipeInsert  *  *      根据id查询管道：  *      pipeIdSelect  *  */

---
+++ model-manager/src/main/java/com/zj/service/ModelServiceImpl.java
@@ -0,0 +1,14 @@
+package com.zj.service;
+
+import com.zj.dao.ModelDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhoujian
+ */
+@Service
+public class ModelServiceImpl implements ModelService {
+    @Autowired
+    private ModelDao modelDao;
+}
---
+++ model-manager/src/main/java/com/zj/web/ModelController.java
@@ -0,0 +1,60 @@
+package com.zj.web;
+
+import com.zj.service.ModelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhoujian
+ * 方法名命名规范
+ * -------------------------------------------------------------------------------
+ * url规范：
+ * 例如：
+ *      管道管理的所用url根都是
+ *      /pipe
+ *      如果根据管道id查询管道
+ *      /pipe/id
+ * -------------------------------------------------------------------------------
+ * controller层类中方法命名规范
+ * 使用规范的请求：
+ *      PostMapping(新增) DeleteMapping(删除) GetMapping(查询) PutMapping(修改)
+ * 动词：
+ *      查询 find
+ *      添加 add
+ *      删除 delete
+ *      修改 modify
+ * 例如：
+ * 添加管道的控制器方法           add  +  pipe  + controller = addPipeController
+ * 根据id查询管道的控制器方法      find + pipe  + id + controller = findPipeIdController
+ * 删除指定id的管道控制器方法      delete + pipe  + id + controller = deletePipeIdController
+ * 修改指定id的官管道的控制器方法   modfiy + pipe  + id + controller = modfiyPipeIdController
+ * ------------------------------------------------------------------------------
+ * service层类中方法名规范： 动词与控制器中的相同
+ * 例如：
+ *      addPipeController ------>  addPipeService
+ * ------------------------------------------------------------------------------
+ * dao层类方法命名规范：
+ * 动词：
+ *      查询：select
+ *      添加：insert
+ *      删除：delete
+ *      修改：update
+ *
+ *例如：
+ *      添加管道：
+ *      addPiepService --------->pipeInsert
+ *      pipe + insert = pipeInsert
+ *
+ *      根据id查询管道：
+ *      pipeIdSelect
+ *
+ */
+@RestController
+@RequestMapping("model")
+public class ModelController {
+    @Autowired
+    private ModelService modelService;
+
+
+}
