7b71dc2b5463f12ef0cb1fcceb4781e0c0cdba56
ZhouJianzj <2998390669@qq.com>
2021-11-08 21:14:46 +0800

    alarm 模块的搭建

---
+++ alarm-manager/src/main/java/com/zj/AlarmApplication.java
@@ -0,0 +1,14 @@
+package com.zj;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableRedisHttpSession
+@MapperScan("com.zj.dao")
+public class AlarmApplication {
+}
