3528f2b5ec1cfd8f32cc26111afa0955a9ccbd93
ZhouJianzj <2998390669@qq.com>
2021-11-08 21:31:40 +0800

    模块全部测试成功！

---
+++ alarm-manager/src/main/java/com/zj/controller/AlarmController.java
@@ -0,0 +1,18 @@
+package com.zj.controller;
+
+
+import com.zj.service.AlarmService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zhoujian
+ */
+@RestController
+@RequestMapping("alarm")
+public class AlarmController {
+    @Resource
+    private AlarmService alarmService;
+}
