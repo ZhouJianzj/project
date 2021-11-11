bf1a6c374dfe31ca4338d90eec1f3e86c70278a1
ZhouJianzj <2998390669@qq.com>
2021-11-11 17:03:57 +0800

    切换分支model

--- model-manager/src/main/java/com/zj/service/ModelServiceImpl.java
+++ model-manager/src/main/java/com/zj/service/ModelServiceImpl.java
@@ -2,7 +2,6 @@ package com.zj.service;
 
 import com.zj.dao.ModelDao;
 import com.zj.entity.CommonResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -39,6 +38,7 @@ public class ModelServiceImpl implements ModelService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(rootPath + file.getOriginalFilename());
         return new CommonResponse<>(200,"上传文件成功！");
     }
 }
