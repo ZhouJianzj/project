8a7927f15aa4c8f3658543d64458ce6d133ab1e5
huxinjie123 <2539323471@qq.com>
2021-11-23 09:43:48 +0800

    Alarm 实体类

--- alarm-manager/src/main/java/com/zj/controller/AlarmController.java
+++ alarm-manager/src/main/java/com/zj/controller/AlarmController.java
@@ -2,7 +2,11 @@ package com.zj.controller;
 
 
 
+import com.zj.entity.Alarm;
+import com.zj.entity.CommonResponse;
 import com.zj.service.AlarmService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +20,12 @@ import javax.annotation.Resource;
 public class AlarmController {
     @Resource
     private AlarmService alarmService;
+
+    /**
+     * 添加报警信息
+     * */
+    @PostMapping("alarm")
+    public CommonResponse<Boolean> insertAlarmController(@RequestBody Alarm alarm){
+        return null;
+    }
 }
