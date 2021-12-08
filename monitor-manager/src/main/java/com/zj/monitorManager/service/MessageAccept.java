fae83808ede391c3c8765daf3e09d6ad395be663
ZhouJianzj <2998390669@qq.com>
2021-12-08 11:01:31 +0800

    找到了随机函数使用之后不生产问题，主要是在数据库数据模拟不对，生产线程导致空指针出现不生产问题

--- monitor-manager/src/main/java/com/zj/monitorManager/service/MessageAccept.java
+++ monitor-manager/src/main/java/com/zj/monitorManager/service/MessageAccept.java
@@ -1,14 +1,11 @@
 package com.zj.monitorManager.service;
 
 import com.zj.monitorManager.entity.Sensor;
-import com.zj.monitorManager.utils.ListMapUtil;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.util.HashMap;
 
 /**
  * @author zhoujian
@@ -27,23 +24,31 @@ public class MessageAccept {
     public void acceptMessage(ConsumerRecord msg){
         //获取value强转为目标对象
         Sensor sensor = (Sensor) msg.value();
-        //获取webSocket连接对象
-        WebSocketService alarmConnection = WebSocketService.concurrentHashMap.get("alarms");
-        System.out.println("==============alarms连接=======" +alarmConnection + "==========");
-        if (alarmConnection != null) {
-                try {
-                    alarmConnection.sendMessage(sensor);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-        }
-        //获取共享haspMap
-        HashMap<String, HashMap<String, HashMap<String, Object>>> hashMapA = ListMapUtil.hashMapA;
-        System.out.println("消费的sensor---------------->" + sensor );
-        //往共享hashMap中存随机生成的sensor报警信息
-        ListMapUtil.updateShareHashMap(hashMapA,sensor);
-        //异常数据插入数据库
-        sensor.getAlarm().setSensorId(sensor.getId());
-        alarmService.insertAlarm(sensor.getAlarm());
+        System.out.println("消费的sensor---------------->" + sensor.getAlarm() );
+//        //获取webSocket连接对象
+//        WebSocketService alarmConnection = WebSocketService.concurrentHashMap.get("alarms");
+//        System.out.println("==============alarms连接=======" +alarmConnection + "==========");
+//        if (alarmConnection != null) {
+//                try {
+//                    alarmConnection.sendMessage(sensor);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//        }
+//        //获取共享haspMap
+//        HashMap<String, HashMap<String, HashMap<String, Object>>> hashMapA = ListMapUtil.hashMapA;
+//        System.out.println("消费的sensor---------------->" + sensor );
+//        //往共享hashMap中存随机生成的sensor报警信息
+//        ListMapUtil.updateShareHashMap(hashMapA,sensor);
+//
+//        //异常数据插入数据库
+//        int currentValue = Integer.parseInt(sensor.getAlarm().getCurrentValue());
+//        SensorModel sensorModel = sensor.getSensorModel();
+//        if (currentValue < sensorModel.getLowThreshold() || currentValue > sensorModel.getHighThreshold() ){
+//            sensor.getAlarm().setSensorId(sensor.getId());
+//            alarmService.insertAlarm(sensor.getAlarm());
+//        }
+
+
     }
 }
